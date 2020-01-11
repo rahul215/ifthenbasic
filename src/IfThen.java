80f07f9c9853147db4c8d9c27784bda8c741632f
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-01-11 07:57:59 -1200

    Initial commit

---
+++ src/IfThen.java
@@ -0,0 +1,11 @@
+public class IfThen {
+    public static void main(String[] args) {
+        int topscore = 100;
+        if(topscore != 100)
+        System.out.println("you are topper!!");
+
+        int Secondtopscore = 60;
+        if(topscore > Secondtopscore && Secondtopscore < 100)
+            System.out.println("Great");
+    }
+}
